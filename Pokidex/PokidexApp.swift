20db3801e6134a2347cef1346f2b30b7062d771f
jdeda <jldeda00@gmail.com>
2022-12-13 14:02:05 -0500

    Initial Commit

---
+++ Pokidex/PokidexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokidexApp.swift
+//  Pokidex
+//
+//  Created by Jesse Deda on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct PokidexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
