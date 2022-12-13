3fb4df297c33133eacdfd3a8516995ec05df4d9c
jdeda <jldeda00@gmail.com>
2022-12-13 14:04:58 -0500

    init state

--- Pokidex/ContentView.swift
+++ Pokidex/ContentView.swift
@@ -1,21 +1,14 @@
-//
-//  ContentView.swift
-//  Pokidex
-//
-//  Created by Jesse Deda on 12/13/22.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
