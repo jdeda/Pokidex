8e5abd0bd670bfded936a0641ea057529e959b30
jdeda <jldeda00@gmail.com>
2022-12-16 18:26:22 -0500

    v5: async stream

--- Pokidex/ContentView.swift
+++ Pokidex/ContentView.swift
@@ -6,16 +6,14 @@ class ViewModel: ObservableObject {
   
   @MainActor
   func onAppear() async  {
-    do {
-      self.pokemon = try await pokemonClient.fetchPokemon()
-    } catch {
-      print(error)
+    for await pokemon in pokemonClient.fetchPokemon() {
+      self.pokemon.append(pokemon)
     }
   }
 }
 
 struct PokemonClient {
-  var fetchPokemon: @Sendable () async throws -> [PokemonModel]
+  var fetchPokemon: @Sendable () -> AsyncStream<PokemonModel>
 }
 
 extension PokemonClient {
@@ -23,84 +21,86 @@ extension PokemonClient {
   // API: https://pokeapi.co/
   static var liveValue: Self {
     .init(fetchPokemon: {
-      struct Response: Codable {
-        let results: [PokemonResult]
-        
-        enum CodingKeys: CodingKey {
-          case results
+      AsyncStream { continuation in
+        Task {
+          struct Response: Codable {
+            let results: [PokemonResult]
+            
+            enum CodingKeys: CodingKey {
+              case results
+            }
+          }
+          struct PokemonResult: Codable {
+            let name: String
+            let url: URL
+          }
+          
+          struct PokemonDetails: Codable {
+            let name: String
+            let sprites: Sprites
+            
+            enum CodingKeys: CodingKey {
+              case name
+              case sprites
+            }
+          }
+          struct Sprites: Codable {
+            let front_default: URL
+            
+            enum CodingKeys: CodingKey {
+              case front_default
+            }
+          }
+          
+          let pokemonURLS = try await JSONDecoder().decode(
+            Response.self,
+            from: URLSession.shared.data(from: URL(string: "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0")!).0
+          )
+            .results
+            .map(\.url)
+          
+          for url in pokemonURLS {
+            let pokemonDetail = try await JSONDecoder().decode(
+              PokemonDetails.self,
+              from: URLSession.shared.data(from: url).0
+            )
+            let pokemon = PokemonModel(
+              id: UUID(),
+              name: pokemonDetail.name,
+              imageURL: pokemonDetail.sprites.front_default
+            )
+            continuation.yield(pokemon)
+          }
         }
       }
-      struct PokemonResult: Codable {
-        let name: String
-        let url: URL
-      }
-      
-      struct PokemonDetails: Codable {
-        let name: String
-        let sprites: Sprites
-        
-        enum CodingKeys: CodingKey {
-          case name
-          case sprites
-        }
-      }
-      struct Sprites: Codable {
-        let front_default: URL
-        
-        enum CodingKeys: CodingKey {
-          case front_default
-        }
-      }
-      
-      let pokemonURLS = try await JSONDecoder().decode(
-        Response.self,
-        from: URLSession.shared.data(from: URL(string: "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0")!).0
-      )
-      .results
-      .map(\.url)
-      
-      var pokemonDetails = [PokemonDetails]()
-      for url in pokemonURLS {
-        let pokemonDetail = try await JSONDecoder().decode(
-          PokemonDetails.self,
-          from: URLSession.shared.data(from: url).0
-        )
-        pokemonDetails.append(pokemonDetail)
-      }
-      
-      let pokemon = pokemonDetails.map {
-        PokemonModel(id: UUID(), name: $0.name, imageURL: $0.sprites.front_default)
-      }
-      
-      print(pokemon)
-      return pokemon
     })
   }
 }
 
-extension PokemonClient {
-  static var previewValue: Self {
-    .init(fetchPokemon: {
-      [
-        .init(
-          id: UUID(),
-          name: "bulbasaur",
-          imageURL: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png")!
-        ),
-        .init(
-          id: UUID(),
-          name: "ivysaur",
-          imageURL: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png")!
-        ),
-        .init(
-          id: UUID(),
-          name: "venusaur",
-          imageURL: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png")!
-        ),
-      ]
-    })
-  }
-}
+//extension PokemonClient {
+//  static var previewValue: Self {
+//    .init(fetchPokemon: {
+//      [
+//        .init(
+//          id: UUID(),
+//          name: "bulbasaur",
+//          imageURL: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png")!
+//        ),
+//        .init(
+//          id: UUID(),
+//          name: "ivysaur",
+//          imageURL: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png")!
+//        ),
+//        .init(
+//          id: UUID(),
+//          name: "venusaur",
+//          imageURL: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png")!
+//        ),
+//      ]
+//    })
+//  }
+//}
+
 struct PokemonModel: Identifiable, Codable {
   let id: UUID
   let name: String
