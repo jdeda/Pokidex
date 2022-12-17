ef92a85d342146445411e207aa1d042437f9e11d
jdeda <jldeda00@gmail.com>
2022-12-16 19:30:53 -0500

    DOCS

--- Pokidex/Dependencies/PokemonClient/PokemonClient+Preview.swift
+++ Pokidex/Dependencies/PokemonClient/PokemonClient+Preview.swift
@@ -5,24 +5,6 @@ extension PokemonClient {
     .init(fetchPokemon: {
       AsyncStream { continuation in
         Task {
-          let models: [Pokemon] = [
-            .init(
-              id: UUID(),
-              name: "bulbasaur",
-              imageURL: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png")!
-            ),
-            .init(
-              id: UUID(),
-              name: "ivysaur",
-              imageURL: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png")!
-            ),
-            .init(
-              id: UUID(),
-              name: "venusaur",
-              imageURL: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png")!
-            ),
-          ]
-          
           for model in models {
             try await Task.sleep(nanoseconds: NSEC_PER_SEC) // Add a delay
             continuation.yield(model)
@@ -33,3 +15,22 @@ extension PokemonClient {
   }
 }
 
+// MARK: - Private
+
+private let models: [PokemonClient.Pokemon] = [
+  .init(
+    id: UUID(),
+    name: "bulbasaur",
+    imageURL: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png")!
+  ),
+  .init(
+    id: UUID(),
+    name: "ivysaur",
+    imageURL: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png")!
+  ),
+  .init(
+    id: UUID(),
+    name: "venusaur",
+    imageURL: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png")!
+  ),
+]
